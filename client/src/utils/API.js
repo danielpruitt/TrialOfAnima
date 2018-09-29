16675aaa1c6ab24c99663d79ec018c30bcd8dc16
danielpruitt <35474097+danielpruitt@users.noreply.github.com>
2018-09-29 14:04:25 -0400

    initialization

---
+++ client/src/utils/API.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+export default {
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Gets the book with the given id
+  getBook: function(id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+  },
+  // Gets all users
+  getUsers: function() {
+    return axios.get("/api/users");
+  },
+  // Gets the user with the given id
+  getUser: function(id) {
+    return axios.get("/api/users/" + id);
+  },
+  // Deletes the user with the given id
+  deleteUser: function(id) {
+    return axios.delete("/api/users/" + id);
+  },
+  // Saves a user to the database
+  saveUser: function(userData) {
+    return axios.post("/api/users", userData);
+  },
+  // Authenticates a user
+  authenticateUser: function(userData) {
+    return axios.post("/auth/login", userData);
+  },
+  // Sign up a user
+  signUp: function(userData) {
+    return axios.post("/auth/signup", userData);
+  }
+};
---
+++ routes/api/books.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// Matches with "/api/books"
+router.route("/")
+  .get(booksController.findAll)
+  .post(booksController.create);
+
+// Matches with "/api/books/count-by-author"
+router
+  .route("/count-by-author")
+  .get(booksController.countsByAuthor);
+
+// Matches with "/api/books/:id"
+router
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.remove);
+
+module.exports = router;
