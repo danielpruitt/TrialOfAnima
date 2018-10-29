095eb1ba444c900c78c8fe53e8ee279f60c920a2
Andrew Park <apark11278@gmail.com>
2018-10-29 16:00:10 -0400

    Added animation when being attacked

--- client/src/components/UICardEnemy/UICardEnemy.js
+++ client/src/components/UICardEnemy/UICardEnemy.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import "./UICard.css";
+import "./UICardEnemy.css";
 
 const styles = {
   card: {
